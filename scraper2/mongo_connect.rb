326cce931ad06c37a047317c937f4c0df4c03209
DrewWeth <dgwetherington@gmail.com>
2015-07-19 00:46:18 -0500

    scripts working now

--- scraper2/mongo_connect.rb
+++ scraper2/mongo_connect.rb
@@ -32,14 +32,12 @@ def execute_scraping (array)
   count = 0
   while (count < array.length)
     full_link = array[count]["full_link"]
-    puts "Scraping (#{count+1}/#{array.count}) #{full_link}"
-    result = execute_scraper2(full_link)
 
+    puts "Scraping (#{ (count + 1) }/#{array.count}) #{full_link}"
+    result = execute_scraper2(full_link)
+    count = count + 1
     send_post(result)
 
-    # puts result
-    # upload_links(result)
-    count = count + 1
   end
   return true
 end
@@ -52,66 +50,46 @@ end
 
 def get_captions(full_link)
   # supply video ID or full YouTube URL from command line
-  if ARGV[0] != nil
-    arg = ARGV[0]
-  else
-    arg = full_link
-  end
+  arg = full_link
+
   if arg =~ /^#{URI::regexp}$/
     link = arg
   else
     link = "http://www.youtube.com/watch?v=#{arg}"
   end
 
+  puts "Link: (#{link})"
+
   # PhantomJS server
-  begin
-    driver = Selenium::WebDriver.for(:remote, :url => "http://localhost:9999")
-    wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
-  rescue Exception => e
-    puts "ERROR: Selenium Error"
-    exit
-  end
+  driver = Selenium::WebDriver.for(:remote, :url => "http://localhost:9999")
+  wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
 
   driver.navigate.to link
 
-  # wait.until { driver.find_element(:id, 'action-panel-overflow-button') }
-
-  # overflow_button = driver.find_element(:id, 'action-panel-overflow-button')
-  overflow_button = driver.find_element :id => "action-panel-overflow-button"
-
+  overflow_button = driver.find_element(:id, 'action-panel-overflow-button')
   overflow_button.click
 
-  begin
-    transcript_button = driver.find_element :class => "action-panel-trigger-transcript"
-    transcript_button.click
+  transcript_button = driver.find_element(:class, 'action-panel-trigger-transcript')
+  transcript_button.click
 
-    # wait for at least one transcript line
-    wait.until { driver.find_element(:id => 'cp-1') }
+  # wait for at least one transcript line
+  wait.until { driver.find_element(:id => 'cp-1') }
 
-    transcript_container = driver.find_element(:id, 'transcript-scrollbox')
+  transcript_container = driver.find_element(:id, 'transcript-scrollbox')
 
-    cc = Nokogiri::HTML(transcript_container.attribute('innerHTML'))
+  cc = Nokogiri::HTML(transcript_container.attribute('innerHTML'))
 
-    transcript_arr = []
-    cc.css('.caption-line').each do |line|
-    	transcript_line = line.css('.caption-line-time').text.gsub("\n", " ") + " " + line.css('.caption-line-text').text.gsub("\n", " ")
-    	transcript_arr << transcript_line
-    end
+  total = "No transcript."
 
-    parsed_transcript = transcript_arr.to_json
-
-    file_name = arg[arg.rindex("=") + 1, arg.size] + "-transcript.txt"
-    puts "TRANSCRIPT-SUCCESS (#{file_name})"
-    return parsed_transcript
-    # if File.write(file_name, parsed_transcript)
-
-  rescue Exception => e
-    puts e
-    puts "ERROR: No transcript available"
+  cc.css('.caption-line').each do |line|
+  	transcript_line = line.css('.caption-line-time').text.gsub("\n", " ") + " " + line.css('.caption-line-text').text.gsub("\n", " ")
+  	total += transcript_line
   end
 
   driver.quit
-  return "No transcript error."
+
+  return total
+
 end
 
 def send_post(hash)
